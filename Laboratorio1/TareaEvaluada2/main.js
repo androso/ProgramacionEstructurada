915a6471b9858e8732605d8f4edd11ab9c0674ba
androso <androso421@gmail.com>
2023-08-24 19:35:24 -0600

    refactor: using for loops

--- Laboratorio1/TareaEvaluada2/main.js
+++ Laboratorio1/TareaEvaluada2/main.js
@@ -1,12 +1,21 @@
 const uniqueSort = () => {
     let conjuntoNumeros = [];
-    while (conjuntoNumeros.length === 0) {
-        const input = prompt("Ingrese un conjunto de números separados por coma (,)\nNota: cualquier texto ingresado será eliminado automáticamente").split(",");
-        conjuntoNumeros = input.map(item => parseInt(item)).filter(item => !isNaN(item));
-        if (conjuntoNumeros.length === 0) {
-            alert("No se ingresó ningún número válido.")
-        }
+    let cantidadDeNumeros = parseInt(prompt("Ingrese la cantidad de números que desea que el conjunto tenga:"));
+
+    while (isNaN(cantidadDeNumeros) || cantidadDeNumeros <= 0) {
+        alert("No se ingresó ningún número válido.");
+        cantidadDeNumeros = parseInt(prompt("Ingrese la cantidad de números que desea que el conjunto tenga:"));
     }
+
+    for (let i = 0; i < cantidadDeNumeros; i++) {
+        let numero = parseInt(prompt("Ingrese el número en posición " + (i + 1) + ":"));
+        while (isNaN(numero)) {
+            alert("No se ingresó ningún número válido.");
+            numero = parseInt(prompt("Ingrese el número en posición " + (i + 1) + ":"));
+        }
+        conjuntoNumeros.push(numero);
+    }
+
     const conjuntoNumerosSinDuplicados = conjuntoNumeros.filter((item, index, array) => array.indexOf(item) === index);
     const conjuntoNumerosSinDuplicadosOrdenados = conjuntoNumerosSinDuplicados.sort((a, b) => a - b);
     alert("El conjunto tras eliminar duplicados y ordenar es: " + conjuntoNumerosSinDuplicadosOrdenados.join(", ") + ".");
